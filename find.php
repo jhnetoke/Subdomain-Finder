dc9cd872542a0befc6fc2bed6853bdcfb8746ef8
Pausi Channel <43511729+404rgr@users.noreply.github.com>
2020-03-12 00:09:49 +0700

    Add files via upload
    Look for subdomains bro.

---
+++ find.php
@@ -0,0 +1,26 @@
+<?
+/*
+  Halo bro? liat² ya?
+  WA : 0895320325423
+*/
+echo "/*
+  Subdomain finder | Simple
+  thaks to api N45HT and my team
+  XploitSec-ID
+*/\n\n";
+$api = "https://api.n45ht.or.id/v1/subdomain-enumeration?domain="; //thanks to n45ht
+echo "input target (tanpa http/https)\n-> ";
+$target = trim(fgets(STDIN));
+echo "\n";
+$satu = $api.$target;
+$get = file_get_contents($satu);
+$get = json_decode($get,true);
+$res = $get['response'];
+$tot = count($get['subdomains']);
+echo "Response: ".$res."\n";
+echo "Message: ".$get['message']."\n";
+echo "Subdomains-> $tot\n";
+foreach($get['subdomains'] as $sub){
+ echo " ".$sub."\n";
+}
+?>
